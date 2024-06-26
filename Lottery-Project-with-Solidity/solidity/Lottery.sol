d6ea5507f419b20ed1a8d4610c94b11f2bad3c77
Dipendra Bhatta <dipenbhat557@gmail.com>
2024-06-26 12:52:38 +0545

    :sparkles: added lottery file

---
+++ Lottery-Project-with-Solidity/solidity/Lottery.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Lottery{
+    address public manager;
+    address payable[] public participants;
+
+    constructor(){
+        manager=msg.sender;
+    }
+
+    receive() external payable {
+        require(msg.value==2 ether);
+        participants.push(payable (msg.sender));
+    }
+
+    function getBalance() public view returns(uint){
+        require(msg.sender==manager);
+        return address(this).balance;
+    }
+
+    function random() public view returns(uint){
+        return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,participants.length)));
+    }
+}
