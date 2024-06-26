03c45d340cd4a9b2c9ee22eb856383f3ac9876ec
Dipendra Bhatta <dipenbhat557@gmail.com>
2024-06-26 22:55:36 +0545

    :sparkles: added select participant function

--- Lottery-Project-with-Solidity/solidity/Lottery.sol
+++ Lottery-Project-with-Solidity/solidity/Lottery.sol
@@ -11,7 +11,7 @@ contract Lottery{
     }
 
     receive() external payable {
-        require(msg.value==2 ether);
+        require(msg.value==1 ether);
         participants.push(payable (msg.sender));
     }
 
@@ -22,5 +22,18 @@ contract Lottery{
 
     function random() public view returns(uint){
         return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,participants.length)));
+    }
+
+    function selectWinner() public {
+        require(msg.sender==manager);
+        require(participants.length>=3);
+        
+        uint r = random();
+        address payable winner;
+        uint index = r % participants.length;
+
+        winner = participants[index];
+        winner.transfer(getBalance());
+        participants = new address payable[](0);
     }
 }
